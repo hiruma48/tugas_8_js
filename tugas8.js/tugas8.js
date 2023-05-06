2da90c0111772fd474225661515e589265f3db5b
tri ariyanto <triyanto129@gmail.com>
2023-05-06 19:27:21 +0700

    tugas 8 js
    mengumpulkan tugas

---
+++ tugas8.js/tugas8.js
@@ -0,0 +1,11 @@
+function panggilobject(){
+    var member = {
+        nama: 'shani',
+        usia : 18 ,
+        generasi : "3",
+        team : "T"
+    }
+    member.team = "k3"
+    console.log(member)
+}
+panggilobject()
